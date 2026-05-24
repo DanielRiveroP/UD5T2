e0b174464deffc6beb2696ef379161f3a699fd74
DanielRiveroP <riveroperezmesa@gmail.com>
2026-05-24 12:16:34 +0100

    Añadidos los cálculos del rectángulo

--- ActividadesAbstracfaces/src/App.java
+++ ActividadesAbstracfaces/src/App.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import net.salesianos.figuras.circulo.Circulo;
+import net.salesianos.figuras.rectangulo.Rectangulo;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,7 +12,17 @@ public class App {
         System.out.println("El área del círculo sabiendo que el radio es: " + radio + " es: " + circulo.calcularArea());
         System.out.println("El perímetro del círculo sabiendo que el radio es: " + radio + " es: " + circulo.calcularPerimetro());
 
+        System.out.println("Introduce la base del rectángulo:");
+        double base = sc.nextDouble();
+        System.out.println("Introduce la altura del rectángulo:");
+        double altura = sc.nextDouble();
+        Rectangulo rectangulo = new Rectangulo(base, altura);
+        System.out.println("El área del rectángulo sabiendo que la base es: " + base + " y la altura es: " + altura + " es: " + rectangulo.calcularArea());
+        System.out.println("El perímetro del rectángulo sabiendo que la base es: " + base + " y la altura es: " + altura + " es: " + rectangulo.calcularPerimetro());
+
+
+        sc.close();
+   
     }
     
-
 }
