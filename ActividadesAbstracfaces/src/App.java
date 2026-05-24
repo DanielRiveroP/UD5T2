027bc318e942636eed1123adf8d5a631b54a5395
DanielRiveroP <riveroperezmesa@gmail.com>
2026-05-24 11:53:34 +0100

    Añadido los calculos del circulo

--- ActividadesAbstracfaces/src/App.java
+++ ActividadesAbstracfaces/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+import net.salesianos.figuras.circulo.Circulo;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Introduce el radio del círculo:");
+        Scanner sc = new Scanner(System.in);
+        double radio = sc.nextDouble();
+        Circulo circulo = new Circulo(radio);
+        System.out.println("El área del círculo sabiendo que el radio es: " + radio + " es: " + circulo.calcularArea());
+        System.out.println("El perímetro del círculo sabiendo que el radio es: " + radio + " es: " + circulo.calcularPerimetro());
+
     }
 }
